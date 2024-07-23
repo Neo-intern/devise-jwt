0d60f5135769b17ce4c6071b93f7c291389c2e21
Neo-intern <neo.intern@bitcube.tech>
2024-07-23 14:26:11 +0200

    API Done

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,7 +16,7 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    if request.headres["Authorization"].present?
+    if request.headers["Authorization"].present?
       jwt_payload = JWT.decode(request.headers["Authorization"].split(" ").last,
       Rails.application.credentials.devise_jwt_secret_key!).first
 
